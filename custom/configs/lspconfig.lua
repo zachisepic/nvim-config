33271a2fef26a89704954439f01a9104633e9327
zachisepic <104940836+zachisepic@users.noreply.github.com>
2024-02-01 18:10:30 -0500

    Add files via upload

---
+++ custom/configs/lspconfig.lua
@@ -0,0 +1,16 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local configs = require 'lspconfig.configs'
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "pyright", "clangd", "lua_ls", "rust_analyzer" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
